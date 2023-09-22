fd84ecd02e2abfdf83639b68314e9f9b733d4387
sa07m <127376011+sa07m@users.noreply.github.com>
2023-09-22 12:34:37 +0530

    redirecting request
    creating a text file

---
+++ redirecting-request10.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const fs = require('fs')
+const server = http.createServer((req,res)=>{
+    const url = req.url
+    const method = req.method
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head><title>Enter message</title></head>')
+        res.write('<body><form action = "/message" method="POST"><input type="text" name="msg"><button type="submit">send</button></form></body>')
+        res.write('</html>')
+        //have to return to make sure below stmts are not executed
+       return res.end()
+    }
+    if(url === '/message' && method==='POST'){
+        fs.writeFileSync('message.txt', 'DUMMY')
+        res.statusCode = 302
+        res.setHeader('Location','/')
+        return res.end()
+       
+    }
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>My first Page</title></head>')
+    res.write('Welcome to my Node Js project</body>')
+    res.write('</html>')
+    res.end()
+})
+server.listen(3000)
